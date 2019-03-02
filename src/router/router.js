aaad1daa5d5189da2ae20daa753c5a2de463bae2
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-03-02 15:35:40 -0500

    AutoDeployCommit

---
+++ src/router/router.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './../views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Home
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: Home
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: Home
+    }
+    ,
+    {
+      path: '/product/:id',
+      name: 'product',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('./../views/About.vue')
+    }
+  ]
+})
