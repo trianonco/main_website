abf9efc5ea0d3db7f72bc78d574c630f232d7b3b
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-03-12 11:39:20 -0500

    AutoDeployCommit

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import store from './store/store'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+
+import { VLazyImagePlugin } from "v-lazy-image";
+
 import './service-workers/registerServiceWorker'
+
+
 
 Vue.config.productionTip = false
 
+Vue.use(VueAwesomeSwiper)
+Vue.use(VLazyImagePlugin);
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,38 @@
 <template>
   <div class="view">
     <header-component/>
+    <swiper :options="swiperOption" ref="mySwiper">
+      <!-- slides -->
+      <swiper-slide>
+        <v-lazy-image src="../assets/Home-Slider/img1.png"/>
+      </swiper-slide>
+      <swiper-slide>
+        <v-lazy-image src="../assets/Home-Slider/img2.png"/>
+      </swiper-slide>
+      <swiper-slide>
+        <v-lazy-image src="../assets/Home-Slider/img3.png"/>
+      </swiper-slide>
+
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+      <div class="swiper-scrollbar" slot="scrollbar"></div>
+    </swiper>
   </div>
 </template>
+
+
+<style>
+.v-lazy-image {
+  filter: blur(10px);
+  transition: filter 0.7s;
+  width: 500px;
+  height: 100px;
+}
+.v-lazy-image-loaded {
+  filter: blur(0);
+  width: 100%;
+}
+</style>
 
 <script>
 // @ is an alias to /src
@@ -12,7 +42,18 @@ export default {
   name: "HomeView",
   components: {
     HeaderComponent
-  }
+  },
+  data() {
+    return {
+      swiperOption: {}
+    };
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.swiper;
+    }
+  },
+  mounted() {}
 };
 </script>
 
